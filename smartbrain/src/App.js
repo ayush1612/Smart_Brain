eb3a10449ef02b785fba7ef253b2f02f3d0c749b
ayush <ujjwal.msrit@gmail.com>
2019-07-29 17:02:51 +0530

    First Commit
    Made the structure for the app

---
+++ smartbrain/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      {
+        // <Logo />
+        // <ImageLinkForm />
+        // <FaceRecognition />
+      }
+    </div>
+  );
+}
+
+export default App;
