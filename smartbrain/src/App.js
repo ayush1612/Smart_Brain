c2908ece33ec05a7930f05ae05757f66c9b86fd6
ayush <ujjwal.msrit@gmail.com>
2019-07-30 22:22:09 +0530

    Structure done

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -1,14 +1,37 @@
 import React from 'react';
 // import logo from './logo.svg';
+import 'tachyons';
 import './App.css';
+import Logo from './components/Logo/Logo';
+import Navigation from './components/Navigation/Navigation';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number:{
+      value:80,
+      density:{
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'  
+       params={particlesOptions}
+          />
+           {/* <Particles/> */}
       <Navigation />
+      <Logo />
+      <Rank/>
+      <ImageLinkForm />
       {
-        // <Logo />
-        // <ImageLinkForm />
         // <FaceRecognition />
       }
     </div>
