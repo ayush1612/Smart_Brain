5c92a3146afdcccd0378e8907c9398656503e0c2
ayush <ujjwal.msrit@gmail.com>
2019-07-31 21:55:34 +0530

    Added the clarifai API and done with states

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{Component} from 'react';
 // import logo from './logo.svg';
 import 'tachyons';
 import './App.css';
@@ -7,11 +7,17 @@ import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+
+const app = new Clarifai.App({
+  apiKey: 'a4fcf383dba74dbd9df3bdc7ea53269d'
+ });
 
 const particlesOptions = {
   particles: {
     number:{
-      value:80,
+      value:100,
       density:{
         enable: true,
         value_area: 800
@@ -20,22 +26,51 @@ const particlesOptions = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles'  
-       params={particlesOptions}
-          />
-           {/* <Particles/> */}
-      <Navigation />
-      <Logo />
-      <Rank/>
-      <ImageLinkForm />
-      {
-        // <FaceRecognition />
-      }
-    </div>
+class App extends Component{
+
+  constructor(){
+   super();
+   this.state = {
+     input: '',
+     imageUrl:''
+   } 
+  }
+
+  onInputChange = (event) =>{
+    this.setState({input:event.target.value});
+  }
+
+  onButtonSubmit = () =>{
+    this.setState({imageUrl:this.state.input})
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    .then(
+    function(response) {
+      // do something with response
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    },
+    function(err) {
+      // there was an error
+    }
   );
+  }
+  render(){
+
+    return (
+      <div className="App">
+        <Particles className='particles'  
+         params={particlesOptions}
+            />
+             {/* <Particles/> */}
+        <Navigation />
+        <Logo />
+        <Rank/>
+        <ImageLinkForm onInputChange={ this.onInputChange } onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        
+      </div>
+    );
+    }
 }
+
 
 export default App;
